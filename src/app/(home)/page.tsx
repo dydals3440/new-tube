f143ac13798f251d3b833d5ceb291145db86d18a
김용민 <dydals3440@gmail.com>
2025-06-03 19:19:39 +0900

    feat: TRPC Setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>I will Load videos in the future</div>;
+import { PageClient } from '@/app/(home)/client';
+import { HydrateClient, prefetch, trpc } from '@/trpc/server';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+function Home() {
+  prefetch(
+    trpc.hello.queryOptions({
+      text: 'matthew',
+    })
+  );
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
+
+export default Home;
